058665806e3cc3505cacbb4c5de73bdc17f62942
abhishekkhatale <abhishekkhatale007@gmail.com>
2025-02-28 17:07:07 +0530

    simple

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import Notesview from './Pages/Notesview.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-     <Notesview/> 
+     <App/> 
     
   </BrowserRouter>
 )
