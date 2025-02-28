4ff14f23e8363aa7ec313e35a17267d659befbd1
Syed Kumail Raza <syedkumailraza28@gmail.com>
2025-02-28 13:53:09 +0530

    events page added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { Routes, Route } from 'react-router-dom'
 import Home from './Pages/Home'
 import Thirdparty from './Pages/Thirdparty'
 import SearchPartner from './Pages/SearchPartner'
+import AllEvents from './Pages/Event'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home />} />
         <Route path="/search-partner" element={<SearchPartner/>} />
+        <Route path="/event-calendar" element={<AllEvents/>} />
       </Routes>
     </div>
   )
